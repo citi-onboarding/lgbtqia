1d2af291e45b674d22285e94c5b34605255d7e12
rosean3 <90877577+rosean3@users.noreply.github.com>
2022-05-25 20:36:44 -0300

    Feature(personalidades): add routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,13 +1,20 @@
 import express from 'express';
 import UserController from '@controllers/UserController'
+import PersonalidadeController from '@controllers/PersonalidadeController'
 
 const routes = express.Router();
 const userController = new UserController();
+const personalidadeController = new PersonalidadeController();
 
 routes.post('/user', userController.create);
 routes.get('/user', userController.get);
 routes.delete('/user/:id', userController.delete);
 routes.put('/user/:id', userController.update);
+
+routes.post('/personalidade', personalidadeController.create);
+routes.get('/personalidade', personalidadeController.get);
+routes.delete('/personalidade/:id', personalidadeController.delete);
+routes.put('/personalidade/:id', personalidadeController.update);
 
 
 export default routes;
