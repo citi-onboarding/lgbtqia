1d0731b799d9e1cb9c1d9cc09171dea15a8d27af
rosean3 <90877577+rosean3@users.noreply.github.com>
2022-05-27 10:39:37 -0300

    Bugfix(personalidades): erase user model, controller and routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,15 +1,8 @@
 import express from 'express';
-import UserController from '@controllers/UserController'
 import PersonalityController from '@controllers/PersonalityController'
 
 const routes = express.Router();
-const userController = new UserController();
 const personalityController = new PersonalityController();
-
-routes.post('/user', userController.create);
-routes.get('/user', userController.get);
-routes.delete('/user/:id', userController.delete);
-routes.put('/user/:id', userController.update);
 
 routes.post('/personality', personalityController.create);
 routes.get('/personality', personalityController.get);
