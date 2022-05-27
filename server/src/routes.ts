76bd6aae5cc283a4d90e1fb13c1e2b155e545e64
PaulaPerazzo <mpperazzosampaio@gmail.com>
2022-05-27 10:10:24 -0300

    feature/news-branch: user removed

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,15 +1,8 @@
 import express from 'express';
-import UserController from '@controllers/UserController'
 import NewsController from '@controllers/NewsController';
 
 const routes = express.Router();
-const userController = new UserController();
 const newsController = new NewsController();
-
-routes.post('/user', userController.create);
-routes.get('/user', userController.get);
-routes.delete('/user/:id', userController.delete);
-routes.put('/user/:id', userController.update);
 
 routes.post('/news', newsController.create);
 routes.get('/news', newsController.get);
