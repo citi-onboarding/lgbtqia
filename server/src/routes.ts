306c76b3a86f38e04b5d1a7c04b1c75f55dead70
bomday <dayanecamilelima@gmail.com>
2022-05-29 01:22:17 -0300

    Feature(Feature/Projects)

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,12 +1,21 @@
 import express from 'express';
 import NewsController from '@controllers/NewsController';
+import ProjectsController from '@controllers/ProjectsController'
 
 const routes = express.Router();
 const newsController = new NewsController();
+const projectsController = new ProjectsController();
 
+//News
 routes.post('/news', newsController.create);
 routes.get('/news', newsController.get);
 routes.delete('/news/:id', newsController.delete);
 routes.put('/news/:id', newsController.update);
 
-export default routes;
+//Projects
+routes.post('/projects', projectsController.create);
+routes.get('/projects', projectsController.get);
+routes.delete('/projects/:id', projectsController.delete);
+routes.put('/projects/:id', projectsController.update);
+
+export default routes; 
