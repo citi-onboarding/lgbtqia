fa3cd7476b1279781ddb27678b5fb1c8b23e512c
PaulaPerazzo <mpperazzosampaio@gmail.com>
2022-06-08 13:39:34 -0300

    Feature(feature/personality-front): back-end integration required

--- client/src/components/personality/index.tsx
+++ client/src/components/personality/index.tsx
@@ -1,24 +1,63 @@
 import React from "react";
-import { PersonalityContainerText, PersonalityTitle, PernalityText, PersonalityContainer, PeronalityCard, PersonalityImage } from "./style";
+import { Container, PersonalityContainerText, PersonalityTitle, PernalityText, PersonalityContainer, PeronalityCard, PersonalityImage, PersonalityName, PersonalityDdescription, PersonalityIconIg, PersonalityLink, PersonalityIconYt, PersonalityButton, ButtonText, ButtonArrow } from "./style";
 import Photo from './img/lorelay.png';
+import Instagram from '../../assets/PersonalityInstagram.svg'
+import Youtube from '../../assets/personalityYoutube.svg'
+import Arrow from '../../assets/personalityArrow.svg'
 
 export const Personality: React.ElementType = () => {
 
     return(
         <>
-            <PersonalityContainerText>
-                <PersonalityTitle>Conheça instituições e pessoas ajudantes da causa</PersonalityTitle>
-                <PernalityText>LGBTQIA+ é o movimento político e social que defende a diversidade e busca mais representatividade e direitos para essa população. A sigla demonstra a luta por mais igualdade e respeito à diversidade.</PernalityText>
-                <PernalityText><strong>Cada letra representa um grupo de pessoas.</strong></PernalityText>
-            </PersonalityContainerText>
+            <Container>
+                <PersonalityContainerText>
+                    <PersonalityTitle>Conheça instituições e pessoas ajudantes da causa</PersonalityTitle>
+                    <PernalityText>LGBTQIA+ é o movimento político e social que defende a diversidade e busca mais representatividade e direitos para essa população. A sigla demonstra a luta por mais igualdade e respeito à diversidade.</PernalityText>
+                    <PernalityText><strong>Cada letra representa um grupo de pessoas.</strong></PernalityText>
+                </PersonalityContainerText>
 
-            <PersonalityContainer>
-                <PeronalityCard>
+                <PersonalityContainer>
+                    <PeronalityCard>
+                        <PersonalityImage src={Photo} alt='lorelayfox'/>
+                        <PersonalityName>Lorelay Fox</PersonalityName>
+                        <PersonalityDdescription>Youtuber, palestrante e dragqueen, Lorelay Fox busca compartilhar conteúdos com base em pesquisa e muita vivência.</PersonalityDdescription>
+                        <PersonalityIconIg src={Instagram}/>
+                        <PersonalityLink href="https://www.instagram.com/lorelay_fox/" target='blank'>@lorelay_fox</PersonalityLink>
+                        <PersonalityIconYt src={Youtube}/>
+                        <PersonalityLink href="https://www.youtube.com/c/lorelayfox" target='blank'>lorelayfox</PersonalityLink>
+                    </PeronalityCard>
+                    <PeronalityCard>
                     <PersonalityImage src={Photo} alt='lorelayfox'/>
-                </PeronalityCard>
-                <PeronalityCard></PeronalityCard>
-            </PersonalityContainer>
-        </>
+                        <PersonalityName>Lorelay Fox</PersonalityName>
+                        <PersonalityDdescription>Youtuber, palestrante e dragqueen, Lorelay Fox busca compartilhar conteúdos com base em pesquisa e muita vivência.</PersonalityDdescription>
+                        <PersonalityIconIg src={Instagram}/>
+                        <PersonalityLink href="https://www.instagram.com/lorelay_fox/" target='blank'>@lorelay_fox</PersonalityLink>
+                        <PersonalityIconYt src={Youtube}/>
+                        <PersonalityLink href="https://www.youtube.com/c/lorelayfox" target='blank'>lorelayfox</PersonalityLink>    
+                    </PeronalityCard>
+                    <PeronalityCard>
+                        <PersonalityImage src={Photo} alt='lorelayfox'/>
+                        <PersonalityName>Lorelay Fox</PersonalityName>
+                        <PersonalityDdescription>Youtuber, palestrante e dragqueen, Lorelay Fox busca compartilhar conteúdos com base em pesquisa e muita vivência.</PersonalityDdescription>
+                        <PersonalityIconIg src={Instagram}/>
+                        <PersonalityLink href="https://www.instagram.com/lorelay_fox/" target='blank'>@lorelay_fox</PersonalityLink>
+                        <PersonalityIconYt src={Youtube}/>
+                        <PersonalityLink href="https://www.youtube.com/c/lorelayfox" target='blank'>lorelayfox</PersonalityLink>
+                    </PeronalityCard>
+                    <PeronalityCard>
+                        <PersonalityImage src={Photo} alt='lorelayfox'/>
+                        <PersonalityName>Lorelay Fox</PersonalityName>
+                        <PersonalityDdescription>Youtuber, palestrante e dragqueen, Lorelay Fox busca compartilhar conteúdos com base em pesquisa e muita vivência.</PersonalityDdescription>
+                        <PersonalityIconIg src={Instagram}/>
+                        <PersonalityLink href="https://www.instagram.com/lorelay_fox/" target='blank'>@lorelay_fox</PersonalityLink>
+                        <PersonalityIconYt src={Youtube}/>
+                        <PersonalityLink href="https://www.youtube.com/c/lorelayfox" target='blank'>lorelayfox</PersonalityLink>    
+                    </PeronalityCard>
 
+                    <PersonalityButton><ButtonText>Conheça mais aliades</ButtonText><ButtonArrow src={Arrow}/></PersonalityButton>
+
+                </PersonalityContainer>
+            </Container>
+        </>
     )
 }
--- client/src/components/personality/style.ts
+++ client/src/components/personality/style.ts
@@ -1,13 +1,18 @@
 import styled from "styled-components";
 import { theme } from '../../styles/theme';
 
+export const Container = styled.div`
+    background: linear-gradient(180deg, rgba(255, 224, 235, 0) 0%, #FFF2F6 96.06%);
+    width: 100%;
+    height: 1152px;
+`;
+
 export const PersonalityContainerText = styled.div`
     display: flex;
     flex-direction: column;
     width: 100%;
     height: 155px;
     align-items: center;
-    background: #FFFFFF;
 `;
 
 export const PersonalityTitle = styled.h1`
@@ -45,17 +50,21 @@ export const PersonalityContainer = styled.div`
     width: 100%;
     /* align-items: center; */
     /* background: linear-gradient(#FFFFFF, #FFF2F6); */
-    background: linear-gradient(180deg, rgba(255, 224, 235, 0) 0%, #FFF2F6 96.06%);
+    /* background: linear-gradient(180deg, rgba(255, 224, 235, 0) 0%, #FFF2F6 96.06%); */
     height: 863px;
     margin-top: 67.73px;
 `;
 
 export const PeronalityCard = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
     width: 556.76px;
     height: 272.22px;
     margin-left: 115.43px;
     /* margin-top: 67.73px; */
     margin-top: 0px;
+    margin-bottom: -70.11px;
     margin-right: -84.04px;
     background: #FCFCFC;
     box-shadow: 2px 2px 20px rgba(0, 0, 0, 0.15);
@@ -63,10 +72,99 @@ export const PeronalityCard = styled.div`
 `;
 
 export const PersonalityImage = styled.img`
-    width: 156.84px;
-    height: 167px;
+    width: 159.84px;
+    height: 170px;
     margin-left: 5px;
     margin-top: 10px;
     filter: drop-shadow(0px 1px 20px rgba(123, 46, 221, 0.46));
     border-radius: 7px;
+`;
+
+export const PersonalityName = styled.h2`
+    width: 202.25px;
+    height: 32.13px;
+    margin-left: 16.15px;
+    margin-top: 26.93px;
+
+    font-family: 'Rubik';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 24px;
+    line-height: 28px;
+
+    color: #101010;
+`;
+
+export const PersonalityDdescription = styled.p`
+    width: 326.21px;
+    height: 94px;
+    margin-left: 180.36px;
+    margin-top: -135px;
+
+    font-family: 'Rubik';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 36px;
+
+    color: #222222;
+`;
+
+export const PersonalityIconIg = styled.img`
+    margin-left: -483.5px;
+    margin-bottom: 32.93px;
+`;
+
+export const PersonalityLink = styled.a`
+    width: 104.01px;
+    height: 25.48px;
+    margin-top: 3.5px;
+    margin-left: 11.66px;
+    margin-bottom: 30.93px;
+
+    font-family: 'Rubik';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 36px;
+
+    color: #222222;
+`;
+
+export const PersonalityIconYt = styled.img`
+    margin-left: 53.67px;
+    margin-bottom: 32.93px;
+`;
+
+export const PersonalityButton = styled.button`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    width: 278.38px;
+    height: 48.41px;
+    margin-left: 115.43px;
+    margin-top: 68.71px;
+    background: #FFFFFF;
+    box-sizing: border-box;
+    border: 0.806897px solid #DDDDDD;
+    box-shadow: 0px 1.61379px 8.87586px rgba(0, 0, 0, 0.09);
+    border-radius: 4.10803px;
+`;
+
+export const ButtonText = styled.p`
+    left: 11.09%;
+    right: 15.63%;
+    top: 25.62%;
+    font-family: 'Sora';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18.5722px;
+    line-height: 23px;
+    text-align: center;
+    color: #101010;
+`;
+
+export const ButtonArrow = styled.img`
+    margin-left: 10px;
 `;
